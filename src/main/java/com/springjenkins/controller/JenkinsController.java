72c8fb68e24e200420e445c97856b6a89d8f969a
Praveen <Praveensonu1999@gmail.com>
2023-12-31 11:39:32 +0530

    New Update

--- src/main/java/com/springjenkins/controller/JenkinsController.java
+++ src/main/java/com/springjenkins/controller/JenkinsController.java
@@ -1,17 +1,15 @@
 package com.springjenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/jenk")
 public class JenkinsController {
 	
 	@GetMapping("/getMessages")
 	public String getMessage()
 	{
-		return "Hi I am From Jenkins Deploy Prjoect";
+		return "Hi I am From Jenkins Deploy Prjoect ";
 	}
 
 }
